795c42f161c21f5cb6cc99c5d50779cbe2c6d2d7
julioct99 <jcasallot01@gmail.com>
2021-09-19 17:42:58 +0200

    Allow full goal definition

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,30 @@
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, Container, TextField, Typography } from '@mui/material';
-import { styled } from '@mui/system';
-import { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 import CustomLineChart from './charts/CustomLineChart';
 import { generateIntermediateStates } from '../shared/utils/okr';
+import { OkrItem, defaultOkrItem } from '../shared/types/okrItem';
 
 const OkrInput = styled('div')`
   margin-bottom: 50px;
   display: flex;
   width: 100%;
   justify-content: space-evenly;
+  align-items: center;
 `;
 
 export function App() {
-  const okrValueNameInputRef = useRef<HTMLInputElement>();
+  const [initState, setInitState] = useState<OkrItem>(defaultOkrItem);
+  const [goalState, setGoalState] = useState<OkrItem>(defaultOkrItem);
+
   const [okrValueName, setOkrValueName] = useState('bodyFat');
   const [intermediateStates, setIntermadiateStates] = useState<any[]>([]);
 
+  const okrValueNameInputRef = useRef<HTMLInputElement>();
+  const okrInitValueInputRef = useRef<HTMLInputElement>();
+  const okrValueInputRef = useRef<HTMLInputElement>();
+
   useEffect(() => {
-    const initState = {
-      date: new Date(2021, 9, 19),
-      value: 19.7,
-    };
-
-    const goalState = {
-      date: new Date(2021, 9, 30),
-      value: 15.0,
-    };
-
     const newIntermediateStates = generateIntermediateStates({
       goalState,
       initState,
@@ -34,7 +32,37 @@ export function App() {
     });
 
     setIntermadiateStates(newIntermediateStates);
-  }, [okrValueName]);
+  }, [okrValueName, initState, goalState]);
+
+  const updateOkr = () => {
+    setOkrValueName(okrValueNameInputRef.current!.value);
+    setInitState((prevState) => ({
+      ...prevState,
+      value: +okrInitValueInputRef.current!.value,
+    }));
+    setGoalState((prevState) => ({
+      ...prevState,
+      value: +okrValueInputRef.current!.value,
+    }));
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.code === 'Enter') updateOkr();
+  };
+
+  const handleInitDateInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInitState((prevState) => ({
+      ...prevState,
+      date: new Date(e.target.value),
+    }));
+  };
+
+  const handleGoalDateInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGoalState((prevState) => ({
+      ...prevState,
+      date: new Date(e.target.value),
+    }));
+  };
 
   return (
     <Container>
@@ -47,21 +75,33 @@ export function App() {
           placeholder='Example: weight, dollars...'
           variant='filled'
           inputRef={okrValueNameInputRef}
-          style={{ width: '400px' }}
+          onKeyDown={handleKeyDown}
         />
-        <Button
-          onClick={() => {
-            if (okrValueNameInputRef.current)
-              setOkrValueName(okrValueNameInputRef.current.value);
-          }}
-          variant='contained'
-        >
-          Set objective
+        <TextField
+          label='Initial value'
+          placeholder='Example: 80, 40...'
+          variant='filled'
+          inputRef={okrInitValueInputRef}
+          onKeyDown={handleKeyDown}
+        />
+        <TextField
+          label='Objective value'
+          placeholder='Example: 45, 100...'
+          variant='filled'
+          inputRef={okrValueInputRef}
+          onKeyDown={handleKeyDown}
+        />
+        <label htmlFor='initDate'>Initial Date</label>
+        <input type='date' name='initDate' onChange={handleInitDateInputChange} />
+        <label htmlFor='goalDate'>Goal Date</label>
+        <input type='date' name='goalDate' onChange={handleGoalDateInputChange} />
+        <Button onClick={updateOkr} variant='contained'>
+          Set
         </Button>
       </OkrInput>
-      {intermediateStates.length > 0 ? (
+      {intermediateStates.length > 0 && (
         <CustomLineChart height={400} data={intermediateStates} />
-      ) : null}
+      )}
     </Container>
   );
 }
--- src/shared/types/okrItem.ts
+++ src/shared/types/okrItem.ts
@@ -2,3 +2,8 @@ export interface OkrItem {
   date: Date;
   value: number;
 }
+
+export const defaultOkrItem: OkrItem = {
+  date: new Date(),
+  value: 0,
+};
