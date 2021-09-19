f08a3bc9a0a5dba7fd70a7d933b12e8af3b2c559
julioct99 <jcasallot01@gmail.com>
2021-09-19 14:44:41 +0200

    First test sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,51 @@
+import { Container } from '@mui/material';
+import CustomLineChart from './components/charts/CustomLineChart';
+
+const initState = {
+  date: new Date(2021, 9, 19),
+  value: 19.7,
+};
+
+const goalState = {
+  date: new Date(2021, 9, 30),
+  value: 15.0,
+};
+
+interface OKRItem {
+  date: Date;
+  value: number;
+}
+
+const intermediateStates: OKRItem[] = [];
+
+const diff = goalState.date.getDate() - initState.date.getDate();
+for (let i = 1; i <= diff; i++) {
+  const date = new Date(initState.date.valueOf());
+  date.setDate(date.getDate() + i);
+
+  const absValueDiff = (Math.abs(goalState.value - initState.value) / diff) * i;
+
+  const diffValue = goalState.value > initState.value ? absValueDiff : -absValueDiff;
+
+  const value: number = +(initState.value + diffValue).toFixed(2);
+  const item: OKRItem = {
+    date,
+    value,
+  };
+  intermediateStates.push(item);
+}
+
+const valueName = 'bodyFat';
+const newIntermediateStates = intermediateStates.map((item) => ({
+  date: item.date,
+  [valueName]: item.value,
+}));
+
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Container>
+      <h1>Hello world</h1>
+      <CustomLineChart height={500} data={newIntermediateStates} />
+    </Container>
+  );
 }
---
+++ src/components/charts/CustomLineChart.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import {
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Line,
+} from 'recharts';
+
+export interface CustomLineChartProps {
+  dataKey?: string;
+  data: Object[];
+  width?: number | string;
+  height?: number | string;
+}
+
+const COLORS = ['#8884d8', '#82ca9d', 'red'];
+
+const renderElement = (element: string, color: string) => (
+  <Line key={element} type='monotone' dataKey={element} stroke={color} />
+);
+
+const CustomLineChart: React.FunctionComponent<CustomLineChartProps> = ({
+  data,
+  dataKey,
+  width,
+  height,
+}) => {
+  const [domain, setDomain] = useState([1000, 1]);
+
+  const defaultWidth = '100%';
+  const defaultHeight = '100%';
+  const defaultDataKey = Object.keys(data[0])[0];
+  const defaultMargin = {
+    top: 5,
+    right: 30,
+    left: 20,
+    bottom: 5,
+  };
+
+  const getLines = () => {
+    const lineNames = Object.keys(data[0]);
+    if (dataKey) {
+      return lineNames.map((el, i) =>
+        el === dataKey ? null : renderElement(el, COLORS[i])
+      );
+    } else {
+      return lineNames.splice(1).map((el, i) => renderElement(el, COLORS[i]));
+    }
+  };
+
+  useEffect(() => {
+    const _lineNames = Object.keys(data[0]);
+    let [_domainLow, _domainHigh] = [1000, 1];
+    _lineNames.forEach((line) => {
+      data.forEach((dataItem: any) => {
+        const item = dataItem[line];
+        if (item < _domainLow) _domainLow = item;
+        if (item > _domainHigh) _domainHigh = item;
+      });
+    });
+    setDomain([_domainLow, _domainHigh]);
+  }, [data]);
+
+  return (
+    <ResponsiveContainer width={width || defaultWidth} height={height || defaultHeight}>
+      <LineChart data={data} margin={defaultMargin}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey={dataKey || defaultDataKey} />
+        <YAxis domain={domain} />
+        <Tooltip />
+        <Legend />
+        {getLines()}
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CustomLineChart;
